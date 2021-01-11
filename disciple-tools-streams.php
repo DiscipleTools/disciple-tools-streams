c5aa42b0f11609de1f581c0be536f65fd04e87b5
Chris Chasm <chris@chasm.solutions>
2021-01-11 11:10:50 -0700

    Remove translation from admin notice

--- disciple-tools-streams.php
+++ disciple-tools-streams.php
@@ -282,9 +282,9 @@ function dt_streams_hook_admin_notice() {
     global $dt_streams_required_dt_theme_version;
     $wp_theme = wp_get_theme();
     $current_version = $wp_theme->version;
-    $message = __( "'Disciple Tools - Streams' plugin requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or make sure it is latest version.", "dt_streams" );
+    $message = "'Disciple Tools - Streams' plugin requires 'Disciple Tools' theme to work. Please activate 'Disciple Tools' theme or make sure it is latest version.";
     if ( $wp_theme->get_template() === "disciple-tools-theme" ){
-        $message .= ' ' . sprintf( esc_html__( 'Current Disciple Tools version: %1$s, required version: %2$s', 'dt_streams' ), esc_html( $current_version ), esc_html( $dt_streams_required_dt_theme_version ) );
+        $message .= ' ' . sprintf( esc_html( 'Current Disciple Tools version: %1$s, required version: %2$s' ), esc_html( $current_version ), esc_html( $dt_streams_required_dt_theme_version ) );
     }
     // Check if it's been dismissed...
     if ( ! get_option( 'dismissed-dt-events', false ) ) { ?>
