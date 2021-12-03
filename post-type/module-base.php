9136c2adc7d4591a903b19d046eb9b18b26dd46a
corsacca <corsacca@gmail.com>
2021-12-03 14:07:05 +0100

    Add icon to location_meta field

--- post-type/module-base.php
+++ post-type/module-base.php
@@ -207,7 +207,8 @@ class DT_Stream_Base extends DT_Module_Base {
                 'type'        => 'location_meta',
                 "tile"      => "details",
                 'mapbox'    => false,
-                'hidden' => true
+                'hidden' => true,
+                "icon" => get_template_directory_uri() . "/dt-assets/images/location.svg?v=2",
             ];
             $fields["contact_address"] = [
                 "name" => __( 'Address', 'disciple-tools-streams' ),
