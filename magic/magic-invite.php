51cf5c047e58dc686cb209d1c78187c1db0c4421
Chris Chasm <chris@chasm.solutions>
2022-10-04 10:52:02 -0600

    Report refactoring

--- magic/magic-invite.php
+++ magic/magic-invite.php
@@ -51,7 +51,6 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
 
     public function header_javascript(){
         ?>
-        ?>
         <script>
             let jsObject = [<?php echo json_encode([
                 'map_key' => DT_Mapbox_API::get_key(),
@@ -60,13 +59,14 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
                 'root' => esc_url_raw( rest_url() ),
                 'nonce' => wp_create_nonce( 'wp_rest' ),
                 'parts' => $this->parts,
+                'title' => get_the_title( $this->parts['post_id'] ),
                 'post_type' => $this->post_type,
                 'trans' => [
                     'add' => __( 'Zume', 'disciple-tools-reporting-app' ),
                 ],
             ]) ?>][0]
             jQuery(document).ready(function($){
-                window.new_stream = ( action, data ) => {
+                window.api_post = ( action, data ) => {
                     return $.ajax({
                         type: "POST",
                         data: JSON.stringify({ action: action, parts: jsObject.parts, data: data }),
@@ -86,41 +86,30 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
                     // add html
                     $('#content').empty().html(`
                         <input type="hidden" id="stream-grid-id" />
-                        <div id="panel1" class="first not-new not-send">
-                            <div class="grid-x">
-                                <div class="cell">
-                                    <button type="button" class="button large expanded show-new">Register a New Stream</button>
-                                </div>
-                                <div class="cell">
-                                    <button type="button" class="button large expanded show-send">Retrieve My Private Link</button>
+                        <div>
+                            <div class="grid-x center">
+                                <div class='cell'>
+                                    <h1>Your Invited to Report on ${jsObject.title}</h1><hr>
+                                    <p>You've been invited to report on your movement work as a contributing reporter for the ${jsObject.title} stream.</p>
+                                    <hr>
                                 </div>
                             </div>
-                        </div>
-
-                        <div id="new-panel" class="new not-first not-send">
                             <div class="grid-x">
                                 <div class="cell panel-note"></div>
                                 <div class="cell">
-                                    <label for="stream_name">Name or nickname of your movement</label>
-                                    <input type="text" id="stream_name" class="required" placeholder="stream name or nickname" />
-                                    <span id="stream-name-error" class="form-error">You're name is required.</span>
-                                </div>
-                                <div class="cell">
-                                    <label for="name">Your name</label>
+                                    <label for="name">Your name *</label>
                                     <input type="text" id="name" class="required" placeholder="Name" />
                                     <span id="name-error" class="form-error">You're name is required.</span>
                                 </div>
-
                                 <div class="cell">
-                                    <label for="email">Email</label>
+                                    <label for="email">Email *</label>
                                     <input type="email" id="email" name="email" placeholder="Email" />
                                     <input type="email" id="e2" name="email" class="required" placeholder="Email" />
                                     <span id="email-error" class="form-error">You're email is required.</span>
                                 </div>
                                 <div class="cell">
                                     <label for="phone">Phone</label>
-                                    <input type="tel" id="phone" name="phone" class="required" placeholder="Phone" />
-                                    <span id="phone-error" class="form-error">You're phone is required.</span>
+                                    <input type="tel" id="phone" name="phone" placeholder="Phone" />
                                 </div>
                                 <div class="cell">
                                     <label for="location">City or address</label>
@@ -128,64 +117,35 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
                                     <span id="phone-error" class="form-error"></span>
                                 </div>
                                 <div class="cell center">
-                                    <button class="button large" id="submit-new">Register</button> <span class="loading-spinner"></span><br>
-                                    <a class="show-first">back</a>
+                                    <button class="button large" id="submit-new">Join as a Reporter</button> <span class="loading-spinner"></span><br>
                                 </div>
                             </div>
                         </div>
-
-                        <div id="send-panel" class="send not-new not-first">
-                            <div class="grid-x">
-                                <div class="cell">
-                                    <label for="email">Email</label>
-                                    <input type="email" id="email-send" name="email" placeholder="Email" />
-                                    <input type="email" id="e2-send" name="email" class="required" placeholder="Email" />
-                                    <span id="email-error-send" class="form-error">You're email is required.</span>
-                                </div>
-                                <div class="cell center">
-                                    <button class="button large" id="submit-send-link">Email me access link</button> <span class="loading-spinner"></span><br>
-                                    <a class="show-first">back</a>
-                                </div>
-                            </div>
-                        </div>
+                        <style>
+                            body {
+                                background-color:white;
+                            }
+                            #wrapper {
+                                max-width: 600px;
+                                margin: 1em auto;
+                            }
+                            #email {
+                                display:none !important;
+                            }
+                            #email-send {
+                                display:none !important;
+                            }
+                        </style>
                     `)
-
-                    // set listeners
-                    let send = $('.send')
-                    let not_send = $('.not-send')
-                    let new_input = $('.new')
-                    let not_new = $('.not-new')
-                    let first = $('.first')
-                    let not_first = $('.not-first')
-                    let note = $('.panel-note')
-                    $('.show-new').on('click', function() {
-                        new_input.show()
-                        not_new.hide()
-                        note.empty()
-                    })
-                    $('.show-send').on('click', function() {
-                        send.show()
-                        not_send.hide()
-                        note.empty()
-                    })
-                    $('.show-first').on('click', function() {
-                        first.show()
-                        not_first.hide()
-                        note.empty()
-                    })
 
                     // listen to buttons
                     $('#submit-new').on('click', function(){
-                        create_streamer()
-                    })
-                    $('#submit-send-link').on('click', function(){
-                        retrieve_link_to_streamer()
+                        join()
                     })
                 } // end function
                 build_modal()
 
-
-                function create_streamer() {
+                function join() {
                     let spinner = $('.loading-spinner')
                     spinner.addClass('active')
 
@@ -227,56 +187,22 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
 
                     let phone_input = $('#phone')
                     let phone = phone_input.val()
-                    if ( ! phone ) {
-                        $('#phone-error').show()
-                        submit_button.removeClass('loading')
-                        email_input.focus(function(){
-                            $('#phone-error').hide()
-                        })
-                        submit_button.prop('disabled', false)
-                        spinner.removeClass('active')
-                        return;
-                    }
-
-                    let stream_name_input = $('#stream_name')
-                    let stream_name = stream_name_input.val()
-                    if ( ! stream_name ) {
-                        $('#stream-name-error').show()
-                        submit_button.removeClass('loading')
-                        email_input.focus(function(){
-                            $('#stream-name-error').hide()
-                        })
-                        submit_button.prop('disabled', false)
-                        spinner.removeClass('active')
-                        return;
-                    }
 
                     let location_input = $('#location')
                     let location = location_input.val()
-                    // if ( ! location ) {
-                    //   $('#location-error').show()
-                    //   submit_button.removeClass('loading')
-                    //   email_input.focus(function(){
-                    //     $('#location-error').hide()
-                    //   })
-                    //   submit_button.prop('disabled', false)
-                    //   spinner.removeClass('active')
-                    //   return;
-                    // }
-
 
                     let form_data = {
                         name: name,
                         email: email,
                         phone: phone,
-                        stream_name: stream_name,
                         location: location
                     }
                     console.log(form_data)
 
-                    window.new_stream( 'register', form_data )
+                    window.api_post( 'register', form_data )
                         .done(function(response){
                             console.log(response)
+
                             let new_panel = $('#new-panel')
                             if ( response.status === 'EMAILED' ) {
                                 new_panel.empty().html(`
@@ -296,75 +222,10 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
                             }
 
                             $('.loading-spinner').removeClass('active')
-                            $('.panel-note').empty()
-                        })
-                }
-
-                function retrieve_link_to_streamer(){
-                    let spinner = $('.loading-spinner')
-                    spinner.addClass('active')
-
-                    let submit_button = $('#submit-send-link')
-                    submit_button.prop('disabled', true)
-
-                    let honey = $('#email-send').val()
-                    if ( honey ) {
-                        submit_button.html('Shame, shame, shame. We know your name ... ROBOT!').prop('disabled', true )
-                        spinner.removeClass('active')
-                        return;
-                    }
-
-                    let email_input = $('#e2-send')
-                    let email = email_input.val()
-                    if ( ! email ) {
-                        $('#email-error-send').show()
-                        submit_button.removeClass('loading')
-                        email_input.focus(function(){
-                            $('#email-error-send').hide()
-                        })
-                        submit_button.prop('disabled', false)
-                        spinner.removeClass('active')
-                        return;
-                    }
-
-                    let form_data = {
-                        email: email
-                    }
-
-                    window.new_stream( 'retrieve', form_data )
-                        .done(function(response){
-                            console.log(response)
-                            if ( response ) {
-                                $('#send-panel').empty().html(`
-                                        Excellent! Go to you email inbox and find your personal link.<br>
-                                      `)
-                                $('.panel-note').empty()
-                            } else {
-                                $('.new').show()
-                                $('.not-new').hide()
-                                $('.panel-note').html('Email not found. Please, register.')
-                            }
-                            $('.loading-spinner').removeClass('active')
-
                         })
                 }
             })
         </script>
-        <style>
-            body {
-                background-color:white;
-            }
-            #wrapper {
-                max-width: 600px;
-                margin: 1em auto;
-            }
-            #email {
-                display:none !important;
-            }
-            #email-send {
-                display:none !important;
-            }
-        </style>
         <?php
     }
 
@@ -379,61 +240,6 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
         <?php
     }
 
-    /* public function body(){
-        DT_Mapbox_API::geocoder_scripts();
-        $parts = $this->parts;
-        $name = get_the_title( $parts['post_id'] );
-        ?>
-        <style>
-            body {
-                background-color:white !important;
-            }
-        </style>
-        <div id="magic-link-wrapper">
-            <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell center">
-                    <h2 id="title">Join the Reporting Team</h2>
-                    You have been invited to participate in the reporting of the <?php echo $name ?> stream.
-                </div>
-            </div>
-            <hr>
-            <div class="grid-x">
-                <div class="cell panel-note"></div>
-                <div class="cell">
-                    <label for="stream_name">Name or nickname of your movement</label>
-                    <input type="text" id="stream_name" class="required" placeholder="stream name or nickname" />
-                    <span id="stream-name-error" class="form-error">You're name is required.</span>
-                </div>
-                <div class="cell">
-                    <label for="name">Your name</label>
-                    <input type="text" id="name" class="required" placeholder="Name" />
-                    <span id="name-error" class="form-error">You're name is required.</span>
-                </div>
-
-                <div class="cell">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Email" />
-                    <input type="email" id="e2" name="email" class="required" placeholder="Email" />
-                    <span id="email-error" class="form-error">You're email is required.</span>
-                </div>
-                <div class="cell">
-                    <label for="phone">Phone</label>
-                    <input type="tel" id="phone" name="phone" class="required" placeholder="Phone" />
-                    <span id="phone-error" class="form-error">You're phone is required.</span>
-                </div>
-                <div class="cell">
-                    <label for="location">City or address</label>
-                    <input type="text" id="location" name="location" placeholder="City or Address" />
-                    <span id="phone-error" class="form-error"></span>
-                </div>
-                <div class="cell center">
-                    <button class="button" id="submit-new">Register</button> <span class="loading-spinner"></span><br>
-                    <a class="show-first">back</a>
-                </div>
-            </div>
-        </div>
-        <?php
-    } */
 
     public function add_endpoints() {
         $namespace = $this->root . '/v1';
@@ -441,16 +247,36 @@ class DT_Streams_Invite_To_Join_Stream extends DT_Magic_Url_Base {
             $namespace, '/' . $this->type, [
                 [
                     'methods'  => 'POST',
-                    'callback' => [ $this, 'register' ],
+                    'callback' => [ $this, 'endpoints' ],
                     'permission_callback' => '__return_true',
                 ],
             ]
         );
     }
 
-    public function register( WP_REST_Request $request ) {
+    public function endpoints( WP_REST_Request $request ) {
         $params = $request->get_params();
+
+        if ( ! isset( $params['parts'],  $params['action'] ) ) {
+            return new WP_Error( __METHOD__, "Missing parameters", [ 'status' => 400 ] );
+        }
+
         $params = dt_recursive_sanitize_array( $params );
+
+        $action = sanitize_text_field( wp_unslash( $params['action'] ) );
+
+        switch ( $action ) {
+            case 'register':
+                return $this->register( $params );
+            default:
+                return new WP_Error( __METHOD__, "Missing valid action", [ 'status' => 400 ] );
+        }
+
+    }
+
+    public function register( $params ) {
+
+
 
         return $params;
     }
@@ -622,7 +448,7 @@ class DT_Streams_Invite_To_Create_Child_Stream extends DT_Magic_Url_Base {
                     $('#submit-send-link').on('click', function(){
                         retrieve_link_to_streamer()
                     })
-                } // end function
+                }
                 build_modal()
 
 
